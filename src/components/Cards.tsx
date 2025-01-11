7211059070832c59c82d369b6dace6e738b9b7ff
Poglia <pedro.poglia@hotmail.com>
2025-01-11 07:27:35 -0300

    fix: bug cards margin padding

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,14 +4,19 @@ export const Cards = () => {
   return (
     <section className=" bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 ">
       <div className="container mx-auto">
-        <h2
-          className="text-4xl md:text-5xl font-bold tracking-tighter 
-        bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text 
-        mt-5 py-10 px-30 flex items-center justify-center text-center"
+         <h2
+            className="text-4xl md:text-5xl font-bold tracking-tighter 
+            bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text 
+            mt-5 py-10 px-4 sm:px-6 lg:px-8 flex items-center justify-center text-center"
         >
-          Seu plano de saúde ou o SUS negou o fornecimento de um tratamento ou
-          medicamento receitado pelo médico?{" "}
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit.?{" "}
         </h2>
+        <p className="text-xl text-[#010D3E] tracking-tight px-4 sm:px-6 lg:px-8 pb-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit.
+        </p>
         <div className="flex md:flex-row flex-col gap-10 items-center justify-center">
           <Card
             title="Perguntas Frequentes"
