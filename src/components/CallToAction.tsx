40e89c2403c6891fa4826978b553302fd15941b9
Poglia <pedro.poglia@hotmail.com>
2025-01-13 15:31:13 -0300

    fix: height sm screen height

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -2,13 +2,13 @@ import { MdOutlineTouchApp } from "react-icons/md";
 
 export const CallToAction = () => {
   return (
-    <section className="bg-white  w-full  h-[600px]">
+    <section className="bg-white  w-full  md:h-[400px] sm:h-[600px]">
       <div className="container mx-auto flex flex-col items-center justify-center w-full">
         <h2 className="h-styles max-w-3xl">
           Você ou um familiar precisa de um medicamento de alto custo? Nós
           podemos ajudar!
         </h2>
-        <button className="btn btn-primary text-2xl md:text-xl mb-10 flex bg-black">
+        <button className="btn btn-primary text-2xl md:text-2xl my-20 flex bg-black">
           <span className="text-transparent animated-gradient-text">
             Avalie meu caso gratuitamente
           </span>
