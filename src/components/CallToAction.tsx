2d9065bf36ffde7b9f675e4267a9cf08c88b2e0f
Poglia <pedro.poglia@hotmail.com>
2025-01-14 14:28:08 -0300

    fix: size padding margin images canva

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -3,13 +3,13 @@ import GostHand from "../assets/gostHand.png";
 
 export const CallToAction = () => {
   return (
-    <section className="bg-white  w-full  md:h-[400px] sm:h-[600px]">
+    <section className="bg-white  w-full">
       <div className="container mx-auto flex flex-col items-center justify-center w-full">
         <h2 className="h-styles max-w-3xl">
           Você ou um familiar precisa de um medicamento de alto custo? Nós
           podemos ajudar!
         </h2>
-        <Image src={GostHand} alt="GostHand" width={350} height={350} />
+        <Image src={GostHand} alt="GostHand" width={350} height={350} className="md:h-full w-auto"/>
         <button className="btn btn-primary text-2xl md:text-2xl my-20 flex bg-black py-5 px-10">
           <span className="text-transparent animated-gradient-text">
             Avalie meu caso gratuitamente
--- src/components/CallToActionFinal.tsx
+++ src/components/CallToActionFinal.tsx
@@ -7,7 +7,7 @@ export const CallToActionFinal = () => {
       <h2 className="h-styles">
         O acesso ao seu tratamento pode estar a um clique de distância.
       </h2>
-      <Image src={Doctors} alt="Doctors" width={350} height={350} className="md:h-full w-auto md:max-w-none md:-left-6  h-30" />
+      <Image src={Doctors} alt="Doctors" width={350} height={350} className="md:h-full w-auto" />
       <button className="btn btn-primary text-xl md:text-2xl my-20 flex bg-black py-5 px-10">
         <span className="text-transparent animated-gradient-text">
         Falar com um Especialista Agora!
